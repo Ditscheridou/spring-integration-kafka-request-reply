7b82b15d3e800b60e902f24ba323fcfd37945ed1
Jonas Dittrich <jonas.dittrich@codecentric.de>
2022-05-05 08:49:25 +0200

    Fixing kafkaTemplate response logging

--- src/main/kotlin/de/jds/demo/DemoApplication.kt
+++ src/main/kotlin/de/jds/demo/DemoApplication.kt
@@ -1,36 +1,24 @@
 package de.jds.demo
 
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate
-import org.springframework.kafka.support.KafkaHeaders
-import org.springframework.util.concurrent.ListenableFutureCallback
-import java.time.Duration
 
 @SpringBootApplication
 class DemoApplication : CommandLineRunner {
+    private val logger = LoggerFactory.getLogger(DemoApplication::class.java.name)
+
     @Autowired
     lateinit var kafkaTemplate: ReplyingKafkaTemplate<String?, RequestDTO, ResponseDTO>
 
     override fun run(vararg args: String?) {
-        val record = ProducerRecord<String?, RequestDTO>("kafkaRequests", RequestDTO().apply { id = 5 }).apply {
-            headers().add(KafkaHeaders.TOPIC, "kafkaRequests".encodeToByteArray())
-                .add(KafkaHeaders.REPLY_TOPIC, "kafkaReplies".encodeToByteArray())
-        }
-        val response = kafkaTemplate.sendAndReceive(record, Duration.ofMillis(4000))
-        response.addCallback(object : ListenableFutureCallback<ConsumerRecord<String?, ResponseDTO>> {
-            override fun onSuccess(result: ConsumerRecord<String?, ResponseDTO>?) {
-                println("Response ${result?.value()}")
-            }
-
-            override fun onFailure(ex: Throwable) {
-                println("Response $ex")
-            }
-        })
+        val record = ProducerRecord<String?, RequestDTO>("kafkaRequests", RequestDTO().apply { id = 5 })
+        val response = kafkaTemplate.sendAndReceive(record).get()
+        logger.info(response.toString())
     }
 }
 
